ae9f0bcd19d104f1d37fe200c53a6de38cc8b5de
Neftali-Samarey <neftali.quintero@cix.csi.cuny.edu>
2024-04-26 11:10:24 -0400

    Initial Commit

---
+++ Blurrify/BlurrifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlurrifyApp.swift
+//  Blurrify
+//
+//  Created by Neftali Samarey on 4/26/24.
+//
+
+import SwiftUI
+
+@main
+struct BlurrifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
