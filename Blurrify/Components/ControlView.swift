18ddaf0ae2ec6e61f354cf681e299797c37d5f50
Neftali-Samarey <neftali.quintero@cix.csi.cuny.edu>
2024-05-11 14:57:41 -0400

    Updated proj directory

---
+++ Blurrify/Components/ControlView.swift
@@ -0,0 +1,18 @@
+//
+//  ControlView.swift
+//  Blurrify
+//
+//  Created by Neftali Samarey on 5/11/24.
+//
+
+import SwiftUI
+
+struct ControlView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ControlView()
+}
