df845b00fbbf7139dd16caa375a8f56004860a65
Neftali-Samarey <neftali.quintero@cix.csi.cuny.edu>
2025-05-27 21:22:01 -0400

    Updated Image to UIImage

--- Blurrify/Views/Main Content View/CanvasContentView.swift
+++ Blurrify/Views/Main Content View/CanvasContentView.swift
@@ -11,24 +11,37 @@ public struct CanvasContentView: View {
 
     @Environment(\.colorScheme) var colorScheme
 
-    private let image: Image
-    private let size: CGSize
+    private let uiImage: UIImage
     private let completion: ((ControlEvent) -> Void)
 
-    public init(image: Image, size: CGSize, completion: @escaping (ControlEvent) -> Void) {
-        self.image = image
-        self.size = size
+    public init(image: UIImage, completion: @escaping (ControlEvent) -> Void) {
+        self.uiImage = image
         self.completion = completion
     }
 
     public var body: some View {
         VStack {
-            Text("Content View")
+            Image(uiImage: uiImage)
+                .resizable()
+                .scaledToFit()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .overlay(alignment: .bottom) {
+            ControlView { event in
+                switch event {
+                case .scribble:
+                    print("Scribble")
+                case .region:
+                    print("Region")
+                case .fullBlur(let blurIntensity):
+                    print("Blur Value: \(blurIntensity)")
+                case .saving:
+                    UIImageWriteToSavedPhotosAlbum(uiImage, nil, nil, nil)
+                case .trash:
+                    completion(.trash)
+                }
+            }
         }
         .background(colorScheme == .dark ? Color.backgroundDarkBlue : Color.white)
     }
-}
-
-#Preview {
-    CanvasContentView(image: .init(""), size: CGSize()) { _ in }
 }
