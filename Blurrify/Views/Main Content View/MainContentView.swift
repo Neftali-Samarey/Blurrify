3ac93a274b36201faf029e36eabd02765dd4100e
Neftali-Samarey <neftali.quintero@cix.csi.cuny.edu>
2025-05-28 21:30:48 -0400

    Cleaned up Main content view

--- Blurrify/Views/Main Content View/MainContentView.swift
+++ Blurrify/Views/Main Content View/MainContentView.swift
@@ -14,18 +14,11 @@ struct MainContentView: View {
     @Environment(\.colorScheme) var colorScheme
 
     @State private var pickerItem: PhotosPickerItem?
-    @State private var selectedImage: Image?
-    @State private var imageToSave: UIImage?
-    @State private var shouldLoadControls: Bool = false
-    @State private var showingAlert = false
-    @State private var showingSavedAlert = true
-    @State private var blurRadius: CGFloat = 0.0
-
-    let alertView = AlertAppleMusic17View(title: "Saved to Photo Album", subtitle: nil, icon: .done)
-    let imageSaver = ImageSaver()
+    @State private var selectedUIImage: UIImage?
+    @State private var uiImageSize: CGSize = CGSize(width: 0, height: 0)
 
     var body: some View {
-        GeometryReader { geometry in
+        NavigationStack {
             ZStack {
                 VStack {
                     PhotosPicker(selection: $pickerItem, matching: .images) {
@@ -44,143 +37,41 @@ struct MainContentView: View {
                     }
                 }
                 .dottedBorder(colorScheme == .dark ? Color.white : Color.primaryBlue.opacity(0.5))
-
-                // This is the overlay view
-                selectedImage?
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .blur(radius: blurRadius, opaque: true)
-                    .onAppear(perform: {
-                        guard selectedImage != nil else {
-                            return
-                        }
-
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.0) {
-                            // TODO: - Animate slide up
-                            withAnimation {
-                                self.shouldLoadControls = true
-                            }
-                        }
-                    })
-
-                if shouldLoadControls {
-                    VStack {
-                        Spacer()
-                        ControlView { isSaving, blurIntensity, isDeleting  in
-                            if let _ = isSaving {
-                                guard let image = selectedImage else { return }
-                                convert(image: image) { editedImage in
-                                    guard let convertedImage = editedImage else { return }
-
-                                    let test = convertedImage.blurredImageWithClippedEdges(inputRadius: blurRadius)
-                                    guard let image = test else { return }
-                                    save(image: image)
-
-                                    //let uiImage = CIImage(image: convertedImage)
-
-                                    // Convert CIImage to UIImage
-                                    /*let context = CIContext()
-                                    if let blurredUIImage = blurredImage.flatMap({
-                                        context.createCGImage($0, from: $0.extent)
-                                    }).flatMap({
-                                        UIImage(cgImage: $0)
-                                    }) {
-                                        self.imageToSave = blurredUIImage
-
-                                        // Save the blurred image to the photo library
-                                        UIImageWriteToSavedPhotosAlbum(blurredUIImage, nil, nil, nil)
-                                    }*/
-                                }
-                                //guard let uiImage = self.selectedImage?.uiImage else { return }
-                            }
-
-                            if let isDeleting = isDeleting {
-                                showingAlert = isDeleting
-                            }
-
-                            if let blurIntensity = blurIntensity {
-                                blurRadius = blurIntensity
-                            }
-                        }
-                            //.alert(isPresent: $showingSavedAlert, view: alertView)
-                    }
-                    .alert("Start all over?", isPresented: $showingAlert) {
-                        // delete
-                        Button("Delete", role: .destructive) {
-                            clear()
-                            DispatchQueue.main.async {
-                                withAnimation {
-                                    // TODO: - Animate slide down
-                                    self.shouldLoadControls = false
-                                }
-                            }
-                        }
-
-                        // cancel
-                        Button("Cancel", role: .cancel) { }
-                    }
-                }
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .onChange(of: pickerItem) {
-                Task {
-                    selectedImage = try await pickerItem?.loadTransferable(type: Image.self)
+            .edgesIgnoringSafeArea(.all)
+            .navigationDestination(isPresented: Binding(
+                get: { selectedUIImage != nil },
+                set: { newValue in
+                    if !newValue { selectedUIImage = nil }
+                }
+            )) {
+                if let selectedImage = selectedUIImage {
+                    CanvasContentView(image: selectedImage) { event in
+                        switch event {
+                        case .trash:
+                            clear()
+                        default: break
+                        }
+                    }
                 }
             }
-            .edgesIgnoringSafeArea(.all)
+            .onChange(of: pickerItem) {
+                Task {
+                    if let data = try? await pickerItem?.loadTransferable(type: Data.self),
+                       let uiImage = UIImage(data: data) {
+                        uiImageSize = uiImage.size
+                        let fixedUIImage = uiImage.normalizedImage()
+                        selectedUIImage = fixedUIImage
+                    }
+                }
+            }
+            .background(colorScheme == .dark ? Color.backgroundDarkBlue : Color.white)
         }
-        .background(colorScheme == .dark ? Color.backgroundDarkBlue : Color.white)
     }
 
     private func clear() {
-        self.blurRadius = 0.0
         self.pickerItem = nil
-        self.selectedImage = nil
+        self.selectedUIImage = nil
     }
-
-    private func save(image: UIImage) {
-        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
-    }
-
-    public func convert(image: Image, callback: @escaping ((UIImage?) -> Void)) {
-        DispatchQueue.main.async {
-            let renderer = ImageRenderer(content: image)
-
-            // to adjust the size, you can use this (or set a frame to get precise output size)
-            // renderer.scale = 0.25
-
-            // for CGImage use renderer.cgImage
-            callback(renderer.uiImage)
-        }
-    }
-
-    func getImageWithBlur(image: UIImage) -> UIImage? {
-
-        let context = CIContext(options: nil)
-
-        guard let currentFilter = CIFilter(name: "CIGaussianBlur") else { return nil }
-
-        let beginImage = CIImage(image: image)
-        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
-        currentFilter.setValue(6.5, forKey: "inputRadius")
-
-        let rect = CGRect(x: 0.0, y: 0.0, width: image.size.width, height: image.size.height)
-
-        guard let output = currentFilter.outputImage?.unpremultiplyingAlpha().settingAlphaOne(in: rect) else { return nil }
-        guard let cgimg = context.createCGImage(output, from: rect) else { return nil }
-
-        print("image.size:    \(image.size)")
-        print("output.extent: \(output.extent)")
-
-        return UIImage(cgImage: cgimg)
-
-    }
-
-//    private func didSelectToSave() -> Bool {
-//        
-//    }
-}
-
-#Preview {
-    MainContentView()
 }
